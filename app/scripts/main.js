4c59af87b5da93735de4c363d221b9221903fc41
Asher <asher.pasha@utoronto.ca>
2016-12-01 22:04:41 -0500

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,31 @@
+/* globals window, AIP, $ */
+var $ = jQuery;
+$(function() {
+    $.xhrPool = [];
+    $.xhrPool.abortAll = function() {
+        $(this).each(function(i, jqXHR) { //  cycle through list of recorded connection
+            jqXHR.abort(); //  aborts connection
+            $.xhrPool.splice(i, 1); //  removes from list by index
+        });
+    }
+    $.ajaxSetup({
+        beforeSend: function(jqXHR) {
+            $.xhrPool.push(jqXHR);
+        }, //  annd connection to list
+        complete: function(jqXHR) {
+            var i = $.xhrPool.indexOf(jqXHR); //  get index for current connection completed
+            if (i > -1) $.xhrPool.splice(i, 1); //  removes from list by index
+        }
+    });
+})
+
+
+window.addEventListener('Agave::ready', function() {
+	if(console){
+		console.log('Agave has been initialized, do something');
+		/* Remove this if you don't want to print the example */
+		var appContext = $('[data-app-name="efp-seq-browser"]');
+		AIP.displayDocs(appContext);
+         
+	}
+});
